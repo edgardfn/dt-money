74cb976ca6f16d6a6b8ebc5f87559742e4425f98
edgardfn <edgardfinotti@yahoo.com.br>
2023-02-23 18:31:02 -0300

    criado tema de cores da aplicação e uma estilizacao global.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './themes/default'
 
 export function App() {
   return (
-    <h1>DT-MONEY</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>DT-MONEY</h1>
+    </ThemeProvider>
   )
 }
-
-
