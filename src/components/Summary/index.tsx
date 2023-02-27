037e55d6b76092d67599f586cb8c84fbb50114be
edgardfn <edgardfinotti@yahoo.com.br>
2023-02-27 16:17:58 -0300

    criado o cálculo das transacoes da sessao de sumario.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -5,7 +5,24 @@ import { SummaryCard, SummaryContainer } from './styles'
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext)
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
 
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -14,7 +31,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -23,7 +40,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 16.000,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -32,7 +49,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$ 1.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
